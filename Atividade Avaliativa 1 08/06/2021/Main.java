0491cda788f346f8d73dfb862fdf6ac2649499d0
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-08 11:22:28 -0300

    Delete Atividade Avaliativa 1 08/06/2021 directory

--- Atividade Avaliativa 1 08/06/2021/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    Cartao cartao = new Cartao();
-        System.out.println(cartao.comprarCafe());
-        System.out.println(cartao.ComprarSaldoDeCafe());
-
-    }
-
-}
