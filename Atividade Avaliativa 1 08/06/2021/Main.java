eecb0ccf4612951a63b5baec90a94c8ec3a6cc62
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-08 11:21:37 -0300

    Create Main.java

---
+++ Atividade Avaliativa 1 08/06/2021/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    Cartao cartao = new Cartao();
+        System.out.println(cartao.comprarCafe());
+        System.out.println(cartao.ComprarSaldoDeCafe());
+
+    }
+
+}
