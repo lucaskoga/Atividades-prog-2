a4d98272219d841ecc06bbd54cd384eb0a5ba0ea
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-07-06 11:32:41 -0300

    Create TestaHeranca.java

---
+++ Atividade de Heranca/TestaHeranca.java
@@ -0,0 +1,8 @@
+package capitulo_7;
+
+public class TestaHeranca {
+    public static void main(String[] args) {
+        Caminhao caminhao = new Caminhao();
+        caminhao.calcularIPVA(2018,22.000);
+    }
+}
