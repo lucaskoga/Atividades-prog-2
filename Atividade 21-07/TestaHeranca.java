e199c12e339b586636a371ce224c961f44cb344e
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-07-22 18:44:40 -0300

    Create TestaHeranca.java

---
+++ Atividade 21-07/TestaHeranca.java
@@ -0,0 +1,10 @@
+public class TestaHeranca {
+    public static void main(String[] args) {
+        Caminhao caminhao = new Caminhao();
+        Veiculo veiculo = new Veiculo("dasda");
+
+        veiculo.equal("dasda");
+
+        veiculo.equal("gsagas");
+    }
+}
