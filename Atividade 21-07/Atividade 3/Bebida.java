25a1a0204955187545bb180e2a332fa0e5111fc1
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-07-22 20:58:31 -0300

    Create Bebida.java

---
+++ Atividade 21-07/Atividade 3/Bebida.java
@@ -0,0 +1,31 @@
+public class Bebida {
+    private String marca;
+    private String nome;
+
+
+
+    public Bebida(){
+        marca = "";
+        nome = "";
+    }
+    public Bebida(String marca,String nome){
+        this.marca = marca;
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
