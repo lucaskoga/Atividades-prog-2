b43c8370bb1f25b695abf2442030ec12f3625cf5
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-08-17 11:11:43 -0300

    Create Produto.java

---
+++ Prog 2 Avaliacão cap 8/Produto.java
@@ -0,0 +1,39 @@
+public abstract class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Produto(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String toString(){
+        return "Codigo: "+ codigo + "Nomedo Produto: "+ nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
