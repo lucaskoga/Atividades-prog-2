d078991cac72073d1fd233d62dc6588da3ef5e0c
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-08-24 12:57:27 -0300

    Update Produto.java

--- Prog 2 Avaliacão cap 8/Produto.java
+++ Prog 2 Avaliacão cap 8/Produto.java
@@ -1,18 +1,33 @@
-public abstract class Produto {
+public abstract class Produto implements Comparable{
+
     private int codigo;
     private String nome;
     private double preco;
 
+    //Criando o Construtor
     public Produto(int codigo, String nome, double preco) {
         this.codigo = codigo;
         this.nome = nome;
         this.preco = preco;
     }
 
+        // ordena pelo codigo
+    @Override
+    public int compareTo(Object o) {
+       Produto outra = (Produto) o;
+        if (this.codigo < outra.getCodigo()){
+           return -1;
+        }
+        if (this.codigo == outra.getCodigo()){
+          return 0;
+     }
+       return 1;
+   }
+   //Reescrevendo o metodo ToString
     public String toString(){
-        return "Codigo: "+ codigo + "Nomedo Produto: "+ nome;
+        return "Codigo: "+ codigo + " Nome do Produto: "+ nome;
     }
-
+    //Todos os get e set
     public int getCodigo() {
         return codigo;
     }
