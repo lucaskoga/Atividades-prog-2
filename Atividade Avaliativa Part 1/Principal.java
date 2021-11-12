78927194b937fe25bd22f88e6f4d7ee162b037c3
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2021-11-12 07:21:30 -0300

    Update Principal.java

--- Atividade Avaliativa Part 1/Principal.java
+++ Atividade Avaliativa Part 1/Principal.java
@@ -1,23 +1,7 @@
 public class Principal {
     public static void main(String[] args) throws ExcecaoPilhaVazia {
-        Pilha pil = new Pilha(5);
+        Pilha pilha = new Pilha();
 
-        for (int i=0; i<7; i++){
-            No n = new No("pil:"+i, i);
-            pil.empilha(n);
-        }
-        System.out.println("Imprime pilha...");
-        pil.imprime();
-
-        pil.desempilha();
-        pil.desempilha();
-
-        pil.imprime();
-        pil.desempilha();
-        pil.desempilha();
-        pil.imprime();
-        pil.desempilha();
-        pil.desempilha();
-
+        pilha.desempilhar();
     }
 }
